650deff6f1996aea2cfe1e97b482c5184cea031c
tasnimAlam <tasnimalamcse@gmail.com>
2021-03-14 13:53:56 +0600

    Update icon list

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5576168f5a9704ffbe92b279aaf56b18');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c55ebf98e56b3142b17744a074c22ece');
->>>>>>> f577c782be3f342f3d5bc35c2680e122baeb9059
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8a8b8755efe84672eeac8d2f705f428f');
