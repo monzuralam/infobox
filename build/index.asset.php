9fca2bc4ba2a4d2dd1e55b04c4951c21c543cbb2
hz-tyfoon <md.tifun@gmail.com>
2021-05-23 18:29:44 +0600

    fixed: helpers custom bgi position unit for tab and mobile set to 'px'

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e6c73eda59f88ee846a59d9d403d0ca4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '189aaba0af63d646b659f0bf62cc6c54');
