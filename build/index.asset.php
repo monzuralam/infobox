aecc863a799d0296b3fb66189b27caa9dc857ef6
hz-tyfoon <md.tifun@gmail.com>
2021-05-25 12:57:32 +0600

    withResBtn included inside background-control

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '93a50f20f54bf093fff5f6778c722668');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fc658f95b29a1c9404b7195f051d0340');
