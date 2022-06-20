2cc4dfed4b14c29429eed1769daa05947c8b3023
Monir <fencemonir@gmail.com>
2022-06-20 18:54:37 +0600

    update style handler to php

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aa6dfcc63ce0045f081a');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6799b96f5a82cbbebf8529debd89ed0f');
->>>>>>> origin
