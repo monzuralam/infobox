d235b07617b66bf793907a7cdcc6a88220b645ba
Monir <fencemonir@gmail.com>
2023-03-29 16:23:17 +0600

    udpate controls

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8644304ddff5f3f04243');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6b193789a25c930b8c1e');
